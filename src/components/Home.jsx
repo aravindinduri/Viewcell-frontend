5a312b8137a67da24a564db1499686c463051f5b
aravindinduri <thearavindinduri@gmail.com>
2024-03-20 19:01:07 +0530

    Refactor codebase to use React Router for routing.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,8 @@
 import React, { useState } from "react";
 import { Parallax } from 'react-parallax';
-import clsx from 'clsx'
-import Logo from "../assets/images/logo.jsx"
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
-import { AiOutlineMenu } from "react-icons/ai";
-import { MdClose } from "react-icons/md";
 import LoginForm from "./login.jsx";
 import RegisterForm from "./register.jsx";
 const Home = () => {
-  const [isSideBarOpen, setOpen] = useState(false)
   const [Toggle, setToggle] = useState(false)
   const [isLogin, setLogin] = useState(false)
   function handleToggle(toggle) {
@@ -20,35 +13,6 @@ const Home = () => {
   return (
 
     <div className="flex flex-col">
-      {/* <div>
-        <nav className="flex flex-row justify-between bg-[#000]">
-          <section>
-            <ul className="flex space-x-4 p-4">
-              <AiOutlineMenu onClick={() => setOpen(true)} className={clsx("text-3xl text-white cursor-pointer sm:hidden transition-all", isSideBarOpen && "invisible")} />
-              <li className="cursor-pointer text-3xl pl-4"><Logo /></li>
-            </ul>
-          </section>
-          {/* side bar 
-          <div className={clsx("sm:grid auto-rows-auto fixed -translate-x-full md:hidden p-4 bg-blue-500/50 z-10 w-screen h-lvh drop-shadow-lg transition-all", isSideBarOpen && "translate-x-0")}>
-            <MdClose onClick={() => setOpen(false)} className="text-4xl text-white mb-4 cursor-pointer" />
-            <div className="flex flex-col gap-9 text-white">
-              <a className="text-3xl cursor-pointer" href="#">About</a>
-              <ul className="text-3xl cursor-pointer flex flex-col gap-4">Contact
-                <a href="https://github.com/ARAVIND-INDURI"><FaGithub /></a>
-                <a href="https://www.linkedin.com/in/aravind-induri-53bb26261/"><FaLinkedin /> </a>
-                <a href="https://mail.google.com/mail/u/1/#inbox"><SiGmail /></a>
-              </ul>
-            </div>
-          </div>
-
-          <section className="flex flex-direction:col">
-            <ul className="flex p-4 space-x-4 ">
-              <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-lg">Sign up</button>
-              <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded-lg">Sign in</button>
-            </ul>
-          </section>
-        </nav>
-      </div> */}
       {/* Body */}
       <div className="flex flex-col overflow-hidden">
         <Parallax bgImage="https://i.postimg.cc/P5YgPs11/neon-22534-960-720.jpg" strength={900} className="flex justify-center items-center w-auto h-lvh bg-cover" >
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element : <Layout/>,
+    children : [
+      {
+        path : "",
+        element : <Home/>
+      }
+    ]
+  },
+]
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
