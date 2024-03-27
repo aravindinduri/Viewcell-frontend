4b23910bafafc1c951b5bb1b2dc20135f856a102
aravindinduri <thearavindinduri@gmail.com>
2024-03-27 22:36:21 +0530

    added Changes

--- src/components/pages/HomePage/UserHome.jsx
+++ src/components/pages/HomePage/UserHome.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function UserHome() {
   return (
-    <div>UserHomejsx</div>
+    <>
+      {/* outer Container */}
+      <div>
+        {/* User Details Container */}
+        <div>
+          <div>
+            <div>
+              <div>upper portion</div>
+              <div>lower Portion</div>
+            </div>
+            <div></div>
+          </div>
+          <div></div>
+        </div>
+        {/* User Body Container */}
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </>
   )
 }
 
