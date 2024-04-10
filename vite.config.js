e1b4a70c0f5e7106a28d5e19ccc4be2eaf7b6ec9
aravindinduri <thearavindinduri@gmail.com>
2024-04-10 18:56:39 +0530

    added changes

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+  // server : {
+  //  proxy : {
+  //   '/api': 'http://localhost:4137'
+  //  }
+//   },
+//   plugins: [react()],
+// })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  server : {
+    proxy : {
+     '/api':'https://viewcell-backend.onrender.com'
+    }
+  },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img'
+          }
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+      },
+    },
+  },
+})
